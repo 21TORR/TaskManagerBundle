ce0f2c224a1e6f0735420d3cf82dd0b1917161c7
Jannik Zschiesche <j.zschiesche@21torr.com>
2024-05-14 14:16:45 +0200

    Add task registry

--- src/Exception/Registry/DuplicateTaskRegisteredException.php
+++ src/Exception/Registry/DuplicateTaskRegisteredException.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Torr\TaskManager\Exception\Registration;
+namespace Torr\TaskManager\Exception\Registry;
 
 use Torr\TaskManager\Exception\TaskManagerException;
 
