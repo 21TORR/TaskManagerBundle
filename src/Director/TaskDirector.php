50f769008d064d4f8be80d70e9fe9725fdc1b468
Jannik Zschiesche <j.zschiesche@21torr.com>
2024-05-16 16:20:08 +0200

    Add run and taskdirector + integration into the logs

---
+++ src/Director/TaskDirector.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace Torr\TaskManager\Director;
+
+use Torr\TaskManager\Model\TaskLogModel;
+use Torr\TaskManager\Task\Task;
+
+final class TaskDirector
+{
+	/**
+	 */
+	public function __construct (
+		private readonly TaskLogModel $logModel,
+	) {}
+
+
+	/**
+	 *
+	 */
+	public function startRun (Task $task) : RunDirector
+	{
+		$log = $this->logModel->getLogForTask($task);
+
+		// create run
+		$run = $this->logModel->createRunForTask($log);
+		$this->logModel->flush();
+
+		return new RunDirector($this->logModel, $run);
+	}
+}
